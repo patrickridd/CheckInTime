6f987453baba7e28bd8597dc88d86ca05cefa545
pattyhackins <patrickridd@yahoo.com>
2016-09-08 14:03:28 -0600

    I save changes to profile via the ProfileVC. I setup a function that is supposed to delete the current user.

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     var loggedInUser: User?
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
     
     
     
@@ -22,13 +23,13 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             return
         }
         imageView.image = user.photo
-
+        
     }
     
     @IBAction func imageTapped(sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
-
+        
         let actionSheet = UIAlertController(title: "Choose an Image Source", message: nil, preferredStyle: .ActionSheet)
         
         
@@ -44,17 +45,55 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         actionSheet.addAction(photoLibraryAction)
         self.presentViewController(actionSheet, animated: true, completion: nil)
     }
-
-    @IBAction func saveButtonTapped(sender: AnyObject) {
     
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+        guard let newImage = imageView.image, loggedInUser = loggedInUser, newImageData = UIImagePNGRepresentation(newImage), loggedInUserRecord = loggedInUser.cloudKitRecord  else {
+            return
+        }
+        
+        loggedInUser.imageData = newImageData
+        
+        if let newName = usernameTextField.text where newName.characters.count > 0{
+            loggedInUser.name = newName
+        }
+        
+        CloudKitManager.cloudKitController.modifyRecords([loggedInUserRecord], perRecordCompletion: { (record, error) in
+            
+        }) { (records, error) in
+            if let error = error {
+                print("Error saving new profile settings. Error: \(error.localizedDescription)")
+            } else {
+                print("Successfully saved profile changes to CloudKit")
+                UserController.sharedController.saveContext()
+                
+            }
+            dispatch_async(dispatch_get_main_queue(), {
+                self.dismissViewControllerAnimated(true, completion: nil)
+            })
+            
+        }
     }
-
+    
     @IBAction func cancelButtonTapped(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-
-    @IBAction func deleteAccountButtonTapped(sender: AnyObject) {
     
+    @IBAction func deleteAccountButtonTapped(sender: AnyObject) {
+        guard let loggedInUser = loggedInUser, loggedInUserRecord = loggedInUser.cloudKitRecord else {
+            return
+        }
+        
+        CloudKitManager.cloudKitController.deleteRecordWithID(loggedInUserRecord.recordID) { (recordID, error) in
+            if let error = error {
+                print("Error Deleting User Record. Error: \(error.localizedDescription)")
+            } else {
+                print("Successfully Deleted User Profile")
+                UserController.sharedController.moc.deleteObject(loggedInUser)
+            }
+            
+            
+        }
+        
     }
     
     
@@ -69,13 +108,13 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
